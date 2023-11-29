f0cd16f01920b8056d4cc3d398e5410854a70eae
Danny Roberts <danzibob@gmail.com>
2023-11-29 16:55:57 +0000

    Initial commit for AoC 2023 in Rust with vscode snippets

---
+++ src/day1.rs
@@ -0,0 +1,21 @@
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &str) -> u32 {
+    0
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT:&str = include_str!("../test_input/2023/day1.txt");
+
+    #[test]
+    fn part1() {
+        
+    }
+}
