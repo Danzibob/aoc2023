570b1ae002dce4e9ca09f4d3bd136b79463ddbe9
Danny Roberts <danzibob@gmail.com>
2023-12-03 13:54:02 +0000

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -15,6 +15,7 @@ struct LineInfo {
 lazy_static! {
     static ref NUMBER:regex::Regex = Regex::new(r"[0-9]+").unwrap();
     static ref SYMBOL:regex::Regex = Regex::new(r"[^0-9\.\n]").unwrap();
+    static ref GEAR:regex::Regex = Regex::new(r"\*").unwrap();
 }
 
 #[inline(always)]
@@ -71,6 +72,65 @@ pub fn solve_part1(input: &str) -> usize {
     }).sum()
 }
 
+#[aoc(day3, part2)]
+pub fn solve_part2(input: &str) -> usize {
+
+    // Collect info about each line
+    let lines_info = input.lines().map(|line|{
+        // Collect part number matches for the line
+        NUMBER.captures_iter(line).map(|m|{
+            let num =  m.get(0).unwrap();
+            PartNum { 
+                value: num.as_str().parse().unwrap(),
+                start: num.start(),
+                end: num.end()
+            }
+        }).collect()
+    }).collect::<Vec<Vec<PartNum>>>();
+
+    let num_lines = input.lines().count();
+
+    const EMPTY:Vec<PartNum> = Vec::new(); // Gross hacky hack
+
+    // Iterate through the collected data
+    input.lines().enumerate().map(|(i, line)|{
+        // Get above and below lines if they exist
+        let above_ = if i>0 {lines_info.get(i-1)} else {None};
+        let below_ = if i<num_lines {lines_info.get(i+1)} else {None};
+        let this = lines_info.get(i).unwrap();
+
+        GEAR.captures_iter(line).map(|m|{
+            let gear = m.get(0).unwrap();
+            // Left of gear
+            let left = this.iter().filter_map(|x| {
+                if x.end == gear.start() 
+                {Some(x.value)} else {None}
+            }).collect::<Vec<usize>>();
+            // Right of gear
+            let right = this.iter().filter_map(|x| {
+                if x.start == gear.end() 
+                {Some(x.value)} else {None}
+            }).collect();
+            // Above gear
+            let above = above_.unwrap_or(&EMPTY).iter().filter_map(|x| {
+                if x.end >= gear.start() && x.start <= gear.end() 
+                {Some(x.value)} else {None}
+            }).collect();
+            // Below gear
+            let below = below_.unwrap_or(&EMPTY).iter().filter_map(|x| {
+                if x.end >= gear.start() && x.start <= gear.end() 
+                {Some(x.value)} else {None}
+            }).collect();
+
+            let values = [left,right,above,below].concat();
+
+            if values.len() == 2 
+            { values.get(0).unwrap() * values.get(1).unwrap()}  
+            else { 0 }
+        }).sum::<usize>()
+    }).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -83,6 +143,11 @@ mod tests {
         assert_eq!(solve_part1(TEST_INPUT), 4361);
         println!("---------------");
         solve_part1(CUSTOM_INPUT);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(solve_part2(TEST_INPUT), 467835);
     }
 
     #[test]
