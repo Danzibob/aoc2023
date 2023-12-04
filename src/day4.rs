11356098905b0ca171b3c081cbf7aa5ef86c2914
Danny Roberts <danzibob@gmail.com>
2023-12-04 17:04:53 +0000

    Day 4 Part 2

--- src/day4.rs
+++ src/day4.rs
@@ -8,9 +8,8 @@ fn parse_card(numbers: &str) -> impl Iterator<Item = usize> + '_{
 
 }
 
-#[aoc(day4, part1)]
-pub fn solve_part1(input: &str) -> usize {
-    input.lines().map(|line| {
+fn count_wins(cards: &str) -> impl Iterator<Item = usize> + '_ {
+    cards.lines().map(|line| {
         let card = 
         line.split_once(": ").unwrap().1
             .split_once(" | ").unwrap();
@@ -18,13 +17,34 @@ pub fn solve_part1(input: &str) -> usize {
         let winning = parse_card(card.0);
         let yours:Vec<usize> = parse_card(card.1).collect();
 
-        let wins = winning.filter(|w| {
+        winning.filter(|w| {
             yours.iter().find(|&x| x == w).is_some()
-        }).count();
+        }).count()
+    })
+}
 
+#[aoc(day4, part1)]
+pub fn solve_part1(input: &str) -> usize {
+    count_wins(input).map(|wins| 
         if wins == 0 {0} else {1 << (wins-1)}
+    ).sum()
+}
 
-    }).sum()
+#[aoc(day4, part2)]
+pub fn solve_part2(input: &str) -> usize {
+    let wins = count_wins(input);
+    let mut num_cards = vec![0; 300]; // Arbitrary big number
+    for (i,card_wins) in wins.enumerate() {
+        num_cards[i] += 1; // Original card
+
+        // Bonus card for each of n wins
+        // x number of this card
+        (1..(card_wins+1)).for_each(|bonus_card|{
+            num_cards[i + bonus_card] += num_cards[i];
+        });
+    }
+
+    num_cards.iter().sum()
 }
 
 #[cfg(test)]
@@ -36,5 +56,10 @@ mod tests {
     #[test]
     fn part1() {
         assert_eq!(solve_part1(TEST_INPUT), 13);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(solve_part2(TEST_INPUT), 30);
     }
 }
